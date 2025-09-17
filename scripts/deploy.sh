4873559dfe53162c7dbed10c922ee4907d19ffae
cove1205 <ganquan007fox@foxmail.com>
2025-09-17 15:31:12 +0800

    feat:脚本更新

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,24 +6,3 @@
 #====================================================
 
 source ./scripts/sh.config
-
-# while [[ $# -gt 0 ]]
-# do
-#     key="$1"
-
-#     case $key in
-#         --platform|-p)
-#         platform="$2"
-#         shift
-#         shift
-#         ;;
-#     esac
-# done
-
-# if [ "$platform" = "android" ]; then
-#     curl -F "file=@$android_apk_path" -F "token=$token" $server_url
-# elif [ "$platform" = "ios" ]; then
-#     curl -F "file=@/xxxxx/xxxxx/xxx/xxx.ipa" http://172.16.10.152:20802/app/upload
-# else
-#     echo "Invalid platform. Please use 'android' or 'ios'."
-# fi
